eac977054395faede40825ddfbb452974e50a1d3
b9b4ymiN <pro.thitipat@gmail.com>
2025-04-26 08:46:52 +0700

    remove auto gen

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -1,17 +1,17 @@
 #!/usr/bin/env zx
 
-import { $ } from 'zx'; // import แค่ $ ก็พอ
+import 'zx/globals';
 
-const type = process.argv[2] ?? "patch"; // ✅ ใช้ process ได้เลย
+const type = process.argv[2]; // <<== ตรงนี้
 
 if (!["patch", "minor", "major"].includes(type)) {
-    console.error(`Invalid version type: ${type}`);
-    process.exit(1);
+  console.error(`Invalid version type: ${type}`);
+  process.exit(1);
 }
 
 await $`git add .`;
 await $`git commit -m "release: ${type} update"`.catch(() => {
-    console.log("No changes to commit, continue...");
+  console.log("No changes to commit, continue...");
 });
 await $`npm version ${type}`;
 await $`git push --follow-tags`;
